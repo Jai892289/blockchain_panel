568df45a8c5ac1c02a6ec59e218caea8da347b0f
Jai892289 <gjai4341@gmail.com>
2026-05-21 11:33:16 +0530

    removed vercel configs

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,35 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
+// import { cookies } from "next/headers";
+// import { redirect } from "next/navigation";
 
-export default async function HomePage() {
-  const cookieStore = await cookies(); // 👈 important
-  const token = cookieStore.get("token");
+// export default async function HomePage() {
+//   const cookieStore = await cookies(); // 👈 important
+//   const token = cookieStore.get("token");
 
-  if (token) {
-    redirect("/dashboard");
-  } else {
-    redirect("/login");
-  }
+//   if (token) {
+//     redirect("/dashboard");
+//   } else {
+//     redirect("/login");
+//   }
+// }
+
+
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function HomePage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      router.replace("/dashboard");
+    } else {
+      router.replace("/login");
+    }
+  }, []);
+
+  return null;
 }
