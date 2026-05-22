d4f592da780eea263a1555e7cb1cc916d34eafe1
Jai892289 <gjai4341@gmail.com>
2026-05-22 11:21:47 +0530

    cors fixed

--- backend/src/utils/app.ts
+++ backend/src/utils/app.ts
@@ -13,7 +13,7 @@ const app = express();
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000", 
+    origin:[ "http://localhost:3000", "https://blockchain-panel.vercel.app"], 
     credentials: true, 
   })
 );
